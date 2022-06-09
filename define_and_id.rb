ee29f5a7a19d77d36c4b15e639301369eb28cf0f
Mary Ballantyne <mballantyne777@gmail.com>
2022-06-09 18:49:57 -0400

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,21 @@
 
 
 "Hello World".include?("Hello")
-
+#The .include? method is called on the string object "Hello World". The argument "Hello" is requesting only that word to be pulled from the string "Hello World"
+# The return value is true
 
 "Hello World".end_with?("Hello")
-
+# The .end_with? method is called on the string object "Hello World". The argument "Hello" is initiating a boolean response to whether or not the string ends in "Hello".
+# The return value is false
 
 "Hello World".end_with?("rld")
-
+# The .endwith? method is calld on the string object "Hello World". The argument "rld" is asking if the string ends in "rld".
+# The return is True
 
 12.even?
-
+# The even? method is called on the integer 12. No arguments are passed. even? is asking if the integer defined is an even number.
+# The return is true
 
 18.next
-
-
+# The next method is called on the integer 18. No arguments are passed. next is requesting to find and return the number following 18.
+# The return is 19
