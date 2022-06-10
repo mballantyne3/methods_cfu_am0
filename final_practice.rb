db031fb8c3b521cc7f9f9dad41b239acd8df9d82
Mary Ballantyne <mballantyne777@gmail.com>
2022-06-09 23:26:08 -0400

    finished self eval and practice

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,59 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
+def greeting
+  puts "How are you"
+end
+p greeting
 
 # What is the return value of your method?
+#method call "How are you"
 # How many arguments did you pass your method?
+# I didn't use any arguments to pass my method in this example
 
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+def custom_greeting(name)
+  "Welcome #{name}"
+end
+p custom_greeting("Mary")
 
 
 # What is the return value of your method?
+# "Welome Mary"
 # How many arguments did you pass your method?
+# 1
 # What data type was your argument(s)?
+# string
 
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
-
-
+def square(num1)
+  num1**2
+end
+puts square(5)
 
 # What is the return value of your method?
+# my return value was 25
 # How many arguments did you pass your method?
+# I passed one argument
 # What data type was your argument(s)?
+# the argument was an integer
 
 
 
 
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
-
-
-
+def greet_person(first, middle, last)
+  puts "Welcome #{first} #{middle} #{last}"
+end
+puts greet_person("mary", "lynn", "ballantyne")
 # What is the return value of your method?
+# "Welcome mary lynn ballantyne"
 # How many arguments did you pass your method?
+# 3
 # What data type was your argument(s)?
-
+# string, string, string
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - How confident do you feel about your use of built-in Ruby methods?
+  Admittedly I'm not feeling enormously confident at this time. I could definitely benefit from further study and practice.
 - How confident do you feel writing simple methods of your own?
+  I'm definitely getting there. Simple methods feel fine, complex methods are taxing.
 - What questions do you have about methods, and what they are capable of? (ask a question!!!)
+  To be honest I'm just processing. Am I able to use a variety of data types in one method? I just need more practice to be honest.
